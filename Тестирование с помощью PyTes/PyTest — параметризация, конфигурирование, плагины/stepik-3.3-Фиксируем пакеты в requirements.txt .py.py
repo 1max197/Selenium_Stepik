dfc62146f163ea27835acd408d7caedca8b7d4b6
MakcP <1max197@mail.ru>
2025-10-28 16:36:28 +0700

    Оставил только тесты с PageObject

--- Тестирование с помощью PyTes/PyTest — параметризация, конфигурирование, плагины/stepik-3.3-Фиксируем пакеты в requirements.txt .py.py
+++
@@ -1,3 +0,0 @@
-#Эта команда сохранит все версии пакетов в специальный файл requirements.txt.
-# pip freeze > requirements.txt # создать файл с зависемостями
-# pip install -r requirements.txt #установить эти пакеты
